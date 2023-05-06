4fbacacef7a603af84615067479e2f8727b71368
karimi <zmanyamyd5@gmail.com>
2023-05-06 14:44:18 +0430

    create PassageModel

---
+++ app/src/main/java/com/example/british_council/PassageModel.kt
@@ -0,0 +1,15 @@
+package com.example.british_council
+
+import android.widget.TextView
+
+class PassageModel(text : String , startTime : Int , endTime : Int ) {
+
+    var text : String? = text
+    var startTime : Int? = startTime
+    var endTime : Int? = endTime
+    var endTimee : TextView? = null
+
+    fun setTextBold(){
+    }
+
+}
