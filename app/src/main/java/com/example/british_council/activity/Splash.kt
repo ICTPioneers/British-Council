2130f2b1831096b0a44821d443180beb4d015981
karimi <zmanyamyd5@gmail.com>
2023-05-07 18:37:39 +0430

    hide status bar

--- app/src/main/java/com/example/british_council/activity/Splash.kt
+++ app/src/main/java/com/example/british_council/activity/Splash.kt
@@ -3,6 +3,7 @@ package com.example.british_council.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import com.example.british_council.R
 import java.util.*
@@ -14,6 +15,7 @@ class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
 //        txt_british
         Timer().schedule(timerTask {
