deef3807acccfd037be56f2fec37cdbce851c701
karimi <zmanyamyd5@gmail.com>
2023-05-13 13:30:43 +0430

    create ApiService

---
+++ app/src/main/java/com/example/british_council/api/ApiService.kt
@@ -0,0 +1,11 @@
+package com.example.british_council.api
+
+import com.example.british_council.model.Data
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("/british-council")
+    fun getPost(): Call<Data>
+
+}
