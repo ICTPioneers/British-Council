1933b6e3380553d608f09fbe5cd976c4bcc71b02
karimi <zmanyamyd5@gmail.com>
2023-05-14 10:57:37 +0330

    fix DatabaseHelper class

--- app/src/main/java/com/example/british_council/database/DatabaseHelper.kt
+++ app/src/main/java/com/example/british_council/database/DatabaseHelper.kt
@@ -2,32 +2,57 @@ package com.example.british_council.database
 
 import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
+import androidx.room.Room.databaseBuilder
 import androidx.room.RoomDatabase
 import com.example.british_council.database.dao.LevelDao
 import com.example.british_council.model.LevelModel
+
 
 @Database(entities = [LevelModel::class], version = 1)
 abstract class DatabaseHelper : RoomDatabase() {
     abstract val dao: LevelDao
 
-    companion object{
+    companion object {
+        private const val DATABASE_NAME = "level_table"
         @Volatile
         private var instance: DatabaseHelper? = null
 
-        fun getInstance(context: Context): DatabaseHelper?{
-            if (instance == null){
-                synchronized(DatabaseHelper::class.java){
-                    instance = Room.databaseBuilder(
-                        context.applicationContext,
-                        DatabaseHelper::class.java,
-                        "notes"
-                    )
-                        .addCallback(StartingLevel(context))
-                        .build()
-                }
+
+        open fun getInstance(context: Context): DatabaseHelper? {
+            if (instance == null) {
+                instance = databaseBuilder(
+                    context.applicationContext,
+                    DatabaseHelper::class.java,
+                    DATABASE_NAME
+                )
+                    .allowMainThreadQueries()
+                    .build()
             }
             return instance
         }
+
+
     }
+
+    //
+//    companion object{
+//        @Volatile
+//        private var instance: DatabaseHelper? = null
+//
+//        fun getInstance(context: Context): DatabaseHelper?{
+//            if (instance == null){
+//                synchronized(DatabaseHelper::class.java){
+//                    instance = Room.databaseBuilder(
+//                        context.applicationContext,
+//                        DatabaseHelper::class.java,
+//                        "notes"
+//                    )
+//                        .addCallback(StartingLevel(context))
+//                        .build()
+//                }
+//            }
+//            return instance
+//        }
+//    }
+
 }
