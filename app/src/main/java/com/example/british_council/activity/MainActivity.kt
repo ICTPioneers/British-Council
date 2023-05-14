7a17bd1b71060e62078fb8904294b055fa8154ac
karimi <zmanyamyd5@gmail.com>
2023-05-14 10:13:31 +0330

    get data from server and set on recyclerview

--- app/src/main/java/com/example/british_council/activity/MainActivity.kt
+++ app/src/main/java/com/example/british_council/activity/MainActivity.kt
@@ -12,21 +12,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.british_council.R
 import com.example.british_council.adapter.SectionAdapter
 import com.example.british_council.api.ApiClient
-import com.example.british_council.api.ApiService
 import com.example.british_council.model.Data
 import com.example.british_council.model.LevelModel
-import com.example.british_council.model.NullHostNameVerifier
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.create
 import java.io.IOException
-import java.security.SecureRandom
-import javax.net.ssl.HttpsURLConnection
-import javax.net.ssl.SSLContext
-import javax.net.ssl.X509TrustManager
 
 class MainActivity : AppCompatActivity() {
     private var txt_hello: TextView? = null
@@ -48,19 +39,6 @@ class MainActivity : AppCompatActivity() {
 
         initID()
         initRecyclerview()
-//        testSever()
-
-        val jsonFileString = getJsonDataFromAsset(applicationContext, "example_leaen_en.json")
-////        Log.i("data", jsonFileString)
-//
-        val gson = Gson()
-        val listPersonType = object : TypeToken<Data>() {}.type
-
-        var persons: Data = gson.fromJson(jsonFileString, listPersonType)
-//        persons.forEachIndexed { idx, person -> Log.i("data", "> Item $idx:\n$person") }
-
-
-
     }
 
 
@@ -85,28 +63,28 @@ class MainActivity : AppCompatActivity() {
         arrayList.add("part 3")
         arrayList.add("part 4")
 
-        adapter = SectionAdapter(this, ArrayList())
-        recyclerView?.adapter = adapter
+//        adapter = SectionAdapter(this, ArrayList())
+//        recyclerView?.adapter = adapter
         testSever()
     }
 
 
      private fun testSever(){
-         var apiService = ApiClient.getClient()?.create(ApiService::class.java)
-         val getApiPost = apiService?.getPost()
-//       var apiService = ApiClient.getClient()?.getPost()
+//         var apiService = ApiClient.getClient()?.create(ApiService::class.java)
+//         val getApiPost = apiService?.getPost()
+         ApiClient.getClient()?.getPost()?.enqueue(object : Callback<Data> {
+             override fun onResponse(call: Call<Data>, response: Response<Data>) {
+                 Log.e("qqq", "onResponse: "+ response.body());
+                 adapter = SectionAdapter(applicationContext, response.body()?.level!!)
+                 recyclerView?.adapter = adapter
+             }
 
-       getApiPost?.enqueue(object : Callback<Data> {
-           override fun onResponse(call: Call<Data>, response: Response<Data>) {
-                Log.e("qqq", "onResponse: "+ response.body());
-               adapter = SectionAdapter(applicationContext, response.body()?.level!!)
-               recyclerView?.adapter = adapter
-           }
+             override fun onFailure(call: Call<Data>, t: Throwable) {
+                 Log.e("qqqq", "onFailure: ", t)
+             }
+         })
 
-           override fun onFailure(call: Call<Data>, t: Throwable) {
-               Log.e("qqqq", "onFailure: ", t)
-           }
-       })
+
    }
 
 
