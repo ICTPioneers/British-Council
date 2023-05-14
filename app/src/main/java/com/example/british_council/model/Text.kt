a3bcc778e96904abafc01152f4318a2cadb6f43a
karimi <zmanyamyd5@gmail.com>
2023-05-14 11:58:51 +0330

    fix id to data model Text

--- app/src/main/java/com/example/british_council/model/Text.kt
+++ app/src/main/java/com/example/british_council/model/Text.kt
@@ -1,7 +1,31 @@
 package com.example.british_council.model
 
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+
+@Entity
 class Text {
-    val text : String? = null
-    var start_time: Int = 0
-    var end_time: Int = 0
+    @PrimaryKey(autoGenerate = true)
+    var id_main :Int? = null
+    var text : String? = null
+    var start_time: Int? = null
+    var end_time: Int? = null
+
+
+    constructor(text: String?, start_time: Int?, end_time: Int? ) {
+        this.text = text
+        this.start_time = start_time
+        this.end_time = end_time
+    }
+
+    @Ignore
+    constructor(id_main: Int?, text: String?, start_time: Int?, end_time: Int?) {
+        this.id_main = id_main
+        this.text = text
+        this.start_time = start_time
+        this.end_time = end_time
+    }
+
+
 }
