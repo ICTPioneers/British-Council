5f0e99863667704c5f04c56f0c4b9e2b683a2e12
karimi <zmanyamyd5@gmail.com>
2023-05-15 10:55:23 +0330

    change type of Array list from string to Text model

--- app/src/main/java/com/example/british_council/adapter/CustomArrayAdapter.kt
+++ app/src/main/java/com/example/british_council/adapter/CustomArrayAdapter.kt
@@ -7,9 +7,10 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import com.example.british_council.R
+import com.example.british_council.model.Text
 
-class CustomArrayAdapter(context: Context, private val list: ArrayList<String>, val pos: Int) :
-    ArrayAdapter<String>(context, R.layout.custom_list_view, list) {
+class CustomArrayAdapter(context: Context, private val list: ArrayList<Text>, val pos: Int) :
+    ArrayAdapter<Text>(context, R.layout.custom_list_view, list) {
 
 
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
@@ -17,7 +18,7 @@ class CustomArrayAdapter(context: Context, private val list: ArrayList<String>, 
         val rowView = inflater.inflate(R.layout.custom_list_view, null, true)
 
         val titleText = rowView.findViewById(R.id.textView_in_listview) as TextView
-        titleText.text = list[position]
+        titleText.text = list[position].text
         titleText.setTextColor(context.resources.getColor(R.color.black))
 
         if (pos == position) {
@@ -27,7 +28,4 @@ class CustomArrayAdapter(context: Context, private val list: ArrayList<String>, 
         return rowView
     }
 
-    fun addList( listt: ArrayList<String>){
-        list.addAll(listt)
-    }
 }
