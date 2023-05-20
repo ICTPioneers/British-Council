e405242756c7e6047d55cadf94014fd938bac3f9
karimi <zmanyamyd5@gmail.com>
2023-05-20 14:17:12 +0330

    create method get Current position

--- app/src/main/java/com/example/british_council/adapter/CustomArrayAdapter.kt
+++ app/src/main/java/com/example/british_council/adapter/CustomArrayAdapter.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
+import androidx.room.Ignore
 import com.example.british_council.R
 import com.example.british_council.helper.App
 import com.example.british_council.model.Text
@@ -14,18 +15,20 @@ import com.example.british_council.model.Text
 class CustomArrayAdapter(
     context: Context,
     private val list: List<Text>,
-    private val currentPos: Int
 ) :
     ArrayAdapter<Text>(context, R.layout.custom_list_view, list) {
 
-    var m: Int? = null
+    private var currentPos: Int? = null
+    @Ignore
+//    constructor() : this()
+
 
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
         val inflater = LayoutInflater.from(parent.context)
         val rowView = inflater.inflate(R.layout.custom_list_view, null, true)
 
         val titleText = rowView.findViewById(R.id.textView_in_listview) as TextView
-        titleText.text = list[position].text
+        titleText.text = list!![position].text
         titleText.setTextColor(context.resources.getColor(R.color.black))
 
 
@@ -48,4 +51,9 @@ class CustomArrayAdapter(
         return rowView
     }
 
+
+
+    fun getCurrentPos(cPos: Int){
+        currentPos = cPos
+    }
 }
