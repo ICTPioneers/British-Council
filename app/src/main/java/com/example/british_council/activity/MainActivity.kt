61cd957a234cfd511d81ced4fc3b9c3c54c6daed
karimi <zmanyamyd5@gmail.com>
2023-05-22 09:57:01 +0330

    clean

--- app/src/main/java/com/example/british_council/activity/MainActivity.kt
+++ app/src/main/java/com/example/british_council/activity/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.british_council.activity
 
 import android.annotation.SuppressLint
-import android.content.DialogInterface
 import android.net.ConnectivityManager
 import android.os.Build
 import android.os.Bundle
@@ -9,23 +8,15 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.view.View
-import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-import com.example.british_council.R
 import com.example.british_council.adapter.SectionAdapter
 import com.example.british_council.api.ApiClient
-import com.example.british_council.database.DatabaseHelper
-import com.example.british_council.databinding.ActivityLevelBinding
 import com.example.british_council.databinding.ActivityMainBinding
 import com.example.british_council.helper.App
 import com.example.british_council.helper.Session
 import com.example.british_council.model.Data
-import com.example.british_council.model.LevelModel
-import com.facebook.shimmer.ShimmerFrameLayout
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -33,10 +24,8 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private var adapter: SectionAdapter? = null
-    private var level: List<LevelModel>? = null
-    private var db: DatabaseHelper? = null
     private var binding: ActivityMainBinding? = null
-    var dialog :AlertDialog? = null
+    private var dialog :AlertDialog? = null
 
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -54,13 +43,6 @@ class MainActivity : AppCompatActivity() {
         val view = binding!!.root
         setContentView(view)
     }
-
-//    private fun initID() {
-//        txt_hello = findViewById(R.id.txt_hello)
-//        recyclerView = findViewById(R.id.rv_main)
-//        swipeRefresh = findViewById(R.id.swipe)
-//        shimmer = findViewById(R.id.shimmer)
-//    }
 
 
     private fun setSwipeRefreshLayout() {
