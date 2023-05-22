65090325696644b44c91e399aa1958b450eda4f1
karimi <zmanyamyd5@gmail.com>
2023-05-22 10:32:10 +0330

    clean

--- app/src/main/java/com/example/british_council/activity/Splash.kt
+++ app/src/main/java/com/example/british_council/activity/Splash.kt
@@ -1,30 +1,23 @@
 package com.example.british_council.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.british_council.R
 import java.util.*
 import kotlin.concurrent.timerTask
 
 class Splash : AppCompatActivity() {
-    var txt : TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
-//        txt_british
         Timer().schedule(timerTask {
             startActivity(Intent(this@Splash, MainActivity::class.java))
             finish()
         }, 2000)
-
     }
-
-
-
 }
