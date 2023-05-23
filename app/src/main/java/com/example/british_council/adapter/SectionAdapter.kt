7fabfda14feeb8ce4f7c4204047554fa652e34e5
karimi <zmanyamyd5@gmail.com>
2023-05-23 09:24:48 +0330

    use databinding in adapter

--- app/src/main/java/com/example/british_council/adapter/SectionAdapter.kt
+++ app/src/main/java/com/example/british_council/adapter/SectionAdapter.kt
@@ -1,48 +1,41 @@
 package com.example.british_council.adapter
 
-import android.content.Context
 import android.content.Intent
 import android.content.res.ColorStateList
+import android.text.TextUtils
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.british_council.R
 import com.example.british_council.activity.LevelActivity
+import com.example.british_council.databinding.ListItemSectionBinding
 import com.example.british_council.helper.App
-import com.example.british_council.helper.Session
 import com.example.british_council.model.Level
-import com.facebook.shimmer.ShimmerFrameLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.gson.Gson
 
-class SectionAdapter(context: Context, val list: List<Level>) :
+class SectionAdapter( private val list: List<Level>) :
     RecyclerView.Adapter<SectionAdapter.MyViewHolder>() {
     var arrayColor: ArrayList<Int> = ArrayList()
     var arrayIcon: ArrayList<Int> = ArrayList()
-    var arrayBack: ArrayList<Int> = ArrayList()
-    var context: Context = context
+    private var arrayBack: ArrayList<Int> = ArrayList()
+    var ll : Level? = null
 
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//        init {
-//            itemView.setOnClickListener { Toast.makeText(, "", Toast.LENGTH_SHORT).show() }
-//        }
+    init {
+        App.toast(App.database.dao.getIdLevel().toString())
+//        if(TextUtils.isEmpty(App.database.dao.getIdLevel(0).toString())) App.toast("level is active null")
+//        else App.toast("level is active " + App.database.dao.getIdLevel(0))
     }
-
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        return MyViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.list_item_section, parent, false)
-        )
+        return MyViewHolder(ListItemSectionBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
 
 
-    override fun onBindViewHolder(holder: SectionAdapter.MyViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         arrayColor.add(R.color.green_2)
         arrayColor.add(R.color.orang)
         arrayColor.add(R.color.gray)
@@ -68,28 +61,38 @@ class SectionAdapter(context: Context, val list: List<Level>) :
 //        holder.itemView.findViewById<TextView>(R.id.tv_part).text = post.title
 //        holder.itemView.findViewById<FloatingActionButton>(R.id.fab).backgroundTintList = ColorStateList.valueOf(context.resources.getColor(R.color.black))
         holder.itemView.findViewById<FloatingActionButton>(R.id.fab).backgroundTintList =
-            ColorStateList.valueOf(context.resources.getColor(arrayColor[position]))
+            ColorStateList.valueOf(App.context.resources.getColor(arrayColor[position]))
         holder.itemView.findViewById<FloatingActionButton>(R.id.fab)
-            .setImageDrawable(context.resources.getDrawable(arrayIcon[position]))
+            .setImageDrawable(App.context.resources.getDrawable(arrayIcon[position]))
 
         holder.itemView.findViewById<LinearLayout>(R.id.linear)
-            .setBackgroundDrawable(context.getDrawable(arrayBack[position]))
+            .setBackgroundDrawable(App.context.getDrawable(arrayBack[position]))
 
 
 
         holder.itemView.setOnClickListener {
 //            App.toast("hello ${list[position].id}")
-            var i = Intent(context,LevelActivity::class.java)
+            var i = Intent(App.context,LevelActivity::class.java)
 //            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             i.flags = Intent.FLAG_ACTIVITY_NEW_TASK
             i.putExtra("level",Gson().toJson(list[position]))
-            context.startActivity(i)
+            App.context.startActivity(i)
         }
     }
-
-
 
     override fun getItemCount(): Int {
         return list.size
     }
+
+
+
+
+
+    class MyViewHolder(private val itemBinding: ListItemSectionBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+//        var title = view.title
+        init {
+
+            }
+    }
+
 }
