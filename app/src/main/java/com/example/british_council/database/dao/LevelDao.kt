58804ec797d8a6da5d1c5d1fc2e94153aa970acb
karimi <zmanyamyd5@gmail.com>
2023-05-23 09:36:43 +0330

    set query update

--- app/src/main/java/com/example/british_council/database/dao/LevelDao.kt
+++ app/src/main/java/com/example/british_council/database/dao/LevelDao.kt
@@ -7,15 +7,31 @@ import com.example.british_council.model.Level
 interface LevelDao {
     @Insert
     fun insert(level: ArrayList<Level>)
-
     @Delete
     fun delete(level: Level)
-
     @Update
     fun update(level: Level)
 
+
+    @Insert
+    fun insertID(id: Level)
+    //    fun insertID(id: ArrayList<Int>)
     @Query("select * from level where id=:id")
     fun getLeve(id: Int): Level
+
+    @Query("select active from level")
+    fun getIdLevel(): List<Int>
+
+
+    @Query("select * from level")
+    fun getAllLeve(): List<Level>
+
+//    @Query("UPDATE level SET password = :pass")
+//    fun updateUserPass(pass: String?)
+
+
+    @Query("UPDATE level SET active = :active WHERE id = :id")
+    fun updateState(active: Int, id: Int)
 
 
 }
