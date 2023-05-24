a61f31fbcc81aa28b19481f9fc207f96dacd330d
karimi <zmanyamyd5@gmail.com>
2023-05-24 14:29:30 +0330

    create state model and change parameter

--- app/src/main/java/com/example/british_council/model/State.kt
+++ app/src/main/java/com/example/british_council/model/State.kt
@@ -7,20 +7,17 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "state")
 class State {
     @PrimaryKey(autoGenerate = true)
-    var id_main :Int? = null
-    var id_level: Int? = null
+    var id :Int? = null
     var state : Int? = null
 
 
     @Ignore
-    constructor(id_main: Int?, id_level: Int?, state: Int?) {
-        this.id_main = id_main
-        this.id_level = id_level
+    constructor(id_main: Int?, state: Int?) {
+        this.id = id_main
         this.state = state
     }
 
-    constructor(id_level: Int?, state: Int?) {
-        this.id_level = id_level
+    constructor( state: Int?) {
         this.state = state
     }
 
