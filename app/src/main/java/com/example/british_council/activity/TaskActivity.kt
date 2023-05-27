4f0d70db407a092ffeb36607ddae337787b9af42
karimi <zmanyamyd5@gmail.com>
2023-05-27 12:14:19 +0330

    add data binding and clean

--- app/src/main/java/com/example/british_council/activity/TaskActivity.kt
+++ app/src/main/java/com/example/british_council/activity/TaskActivity.kt
@@ -11,93 +11,129 @@ import android.view.DragEvent
 import android.view.View
 import android.view.View.DragShadowBuilder
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.british_council.R
+import com.example.british_council.databinding.ActivityTaskBinding
 
-class TaskActivity : AppCompatActivity() , View.OnLongClickListener , View.OnDragListener{
+class TaskActivity : AppCompatActivity(), View.OnLongClickListener, View.OnDragListener {
+
+    private var binding: ActivityTaskBinding? = null
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task)
-        val btn1 = findViewById<View>(R.id.red) as Button
-        btn1.tag = "DRAGGABLE BUTTON 1"
-        btn1.setOnLongClickListener(this)
-        val btn2 = findViewById<View>(R.id.blue) as Button
-        btn2.tag = "DRAGGABLE BUTTON 2"
-        val btn3 = findViewById<View>(R.id.cat) as Button
-        btn3.tag = "DRAGGABLE BUTTON 3"
 
-        val btn4 = findViewById<View>(R.id.dog) as Button
-        btn4.tag = "DRAGGABLE BUTTON 4"
-
-        btn2.setOnLongClickListener(this)
-        btn3.setOnLongClickListener(this)
-        btn4.setOnLongClickListener(this)
-
-        findViewById<View>(R.id.layout1).setOnDragListener(this)
-        findViewById<View>(R.id.color).setOnDragListener(this)
-        findViewById<View>(R.id.animal).setOnDragListener(this)
-
+        initBinding()
+        setTag()
+        setOnLongClickListener()
+        setDragListener()
+        clicked()
     }
 
-      override fun onDrag(v: View?, event: DragEvent?): Boolean {
-          val action: Int = event!!.action
-          when (action) {
-              DragEvent.ACTION_DRAG_STARTED -> {
-                  return event.clipDescription
-                          .hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
-              }
-              DragEvent.ACTION_DRAG_ENTERED -> {
-                  v?.background!!.setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_IN)
-                  v.invalidate()
-                  return true
-              }
-              DragEvent.ACTION_DRAG_LOCATION -> return true
-              DragEvent.ACTION_DRAG_EXITED -> {
-                  v!!.background.clearColorFilter()
-                  v.invalidate()
-                  return true
-              }
-              DragEvent.ACTION_DROP -> {
-                  val item: ClipData.Item = event.clipData.getItemAt(0)
-                  val dragData = item.text.toString()
-                  Toast.makeText(this, "Dragged data is $dragData", Toast.LENGTH_SHORT).show()
-                  v!!.background.clearColorFilter()
-                  v.invalidate()
-                  val vw = event.localState as View
-                  val owner = vw.parent as ViewGroup
-                  owner.removeView(vw)
-                  val container = v as LinearLayout
-                  container.addView(vw)
-                  vw.visibility = View.VISIBLE
-                  return true
-              }
-              DragEvent.ACTION_DRAG_ENDED -> {
-                  v!!.background.clearColorFilter()
-                  v.invalidate()
-                  if (event.result) Toast.makeText(
-                      this,
-                      "The drop was handled.",
-                      Toast.LENGTH_SHORT
-                  ).show() else Toast.makeText(this, "The drop didn't work.", Toast.LENGTH_SHORT)
-                      .show()
-                  return true
-              }
-              else -> Log.e("DragDrop Example", "Unknown action type received by OnDragListener.")
-          }
-          return false
-      }
 
-      override fun onLongClick(v: View?): Boolean {
-          val item = ClipData.Item(v?.tag as CharSequence)
-          val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
-          val data = ClipData(v.tag.toString(), mimeTypes, item)
-          val dragshadow = DragShadowBuilder(v)
-          v.startDrag(data, dragshadow, v, 0)
-          return true
-      }
+    private fun initBinding() {
+        binding = ActivityTaskBinding.inflate(layoutInflater)
+        val view = binding!!.root
+        setContentView(view)
+    }
+
+    private fun setTag(){
+        binding!!.red.tag = "DRAGGABLE BUTTON 1"
+        binding!!.blue.tag = "DRAGGABLE BUTTON 2"
+        binding!!.cat.tag = "DRAGGABLE BUTTON 3"
+        binding!!.dog.tag = "DRAGGABLE BUTTON 4"
+    }
+
+    private fun setOnLongClickListener(){
+        binding!!.red.setOnLongClickListener(this)
+        binding!!.blue.setOnLongClickListener(this)
+        binding!!.cat.setOnLongClickListener(this)
+        binding!!.dog.setOnLongClickListener(this)
+    }
+
+
+    private fun setDragListener(){
+        binding!!.layoutMain.setOnDragListener(this)
+        binding!!.colorLayout.setOnDragListener(this)
+        binding!!.animalLayout.setOnDragListener(this)
+    }
+
+    override fun onDrag(v: View?, event: DragEvent?): Boolean {
+        when (event!!.action) {
+            DragEvent.ACTION_DRAG_STARTED -> {
+                return event.clipDescription
+                    .hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            }
+            DragEvent.ACTION_DRAG_ENTERED -> {
+                v?.background!!.setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_IN)
+                v.invalidate()
+                return true
+            }
+            DragEvent.ACTION_DRAG_LOCATION -> return true
+            DragEvent.ACTION_DRAG_EXITED -> {
+                v!!.background.clearColorFilter()
+                v.invalidate()
+                return true
+            }
+            DragEvent.ACTION_DROP -> {
+                val item: ClipData.Item = event.clipData.getItemAt(0)
+                val dragData = item.text.toString()
+//                  Toast.makeText(this, "Dragged data is $dragData", Toast.LENGTH_SHORT).show()
+
+                v!!.background.clearColorFilter()
+                v.invalidate()
+                val vw = event.localState as View
+                val owner = vw.parent as ViewGroup
+                owner.removeView(vw)
+                val container = v as LinearLayout
+                container.addView(vw)
+                vw.visibility = View.VISIBLE
+                return true
+            }
+
+            DragEvent.ACTION_DRAG_ENDED -> {
+                v!!.background.clearColorFilter()
+                v.invalidate()
+                if (event.result) Toast.makeText(
+                    this,
+                    "The drop was handled.",
+                    Toast.LENGTH_SHORT
+                ).show() else Toast.makeText(this, "The drop didn't work.", Toast.LENGTH_SHORT)
+                    .show()
+                return true
+            }
+            else -> Log.e("DragDrop Example", "Unknown action type received by OnDragListener.")
+        }
+        return false
+    }
+
+    override fun onLongClick(v: View?): Boolean {
+        val item = ClipData.Item(v?.tag as CharSequence)
+        val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
+        val data = ClipData(v.tag.toString(), mimeTypes, item)
+        val dragshadow = DragShadowBuilder(v)
+        v.startDrag(data, dragshadow, v, 0)
+        return true
+    }
+
+    private fun clicked() {
+        binding!!.colorGroup?.setOnClickListener { selectFromLayout(binding!!.colorLayout!!) }
+    }
+
+    private fun selectFromLayout(ll: LinearLayout) {
+        var tagAnimal = arrayOf("DRAGGABLE BUTTON 1", "DRAGGABLE BUTTON 2")
+        val childCount = ll.childCount
+        for (i in 0 until childCount) {
+            val view = ll.getChildAt(i)
+            if (view.tag in tagAnimal) {
+                view.setBackgroundColor(resources.getColor(R.color.green_lite))
+            } else view.setBackgroundColor(resources.getColor(R.color.red))
+        }
+    }
+
+
 }
+
