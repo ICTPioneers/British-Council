61b47a00c3226e885510499e0491564e0506487b
karimi <zmanyamyd5@gmail.com>
2023-05-28 11:07:59 +0330

    fix

--- app/src/main/java/com/example/british_council/adapter/SectionAdapter.kt
+++ app/src/main/java/com/example/british_council/adapter/SectionAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.british_council.adapter
 
-import android.content.Intent
 import android.content.res.ColorStateList
 import android.location.GnssAntennaInfo.Listener
 import android.view.LayoutInflater
@@ -9,16 +8,14 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.british_council.R
-import com.example.british_council.activity.LevelActivity
 import com.example.british_council.databinding.ListItemSectionBinding
 import com.example.british_council.helper.App
 import com.example.british_council.model.Level
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.gson.Gson
 
-class SectionAdapter( private var list: List<Level> , listener : Listener) :
+class SectionAdapter(private var list: List<Level>, listener: Listener) :
     RecyclerView.Adapter<SectionAdapter.MyViewHolder>() {
-    var listener :Listener = listener
+    var listener = listener
 
 
     interface Listener {
@@ -26,9 +23,14 @@ class SectionAdapter( private var list: List<Level> , listener : Listener) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        return MyViewHolder(ListItemSectionBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        return MyViewHolder(
+            ListItemSectionBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
-
 
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
@@ -36,12 +38,12 @@ class SectionAdapter( private var list: List<Level> , listener : Listener) :
         var item = holder.itemView
         var model = list[position]
 
-        item.findViewById<TextView>(R.id.level_name).text =  list[position].name
-        item.findViewById<TextView>(R.id.part_name).text =  list[position].desc
+        item.findViewById<TextView>(R.id.level_name).text = list[position].name
+        item.findViewById<TextView>(R.id.part_name).text = list[position].desc
 
 
-        when(model.states){
-            1->{
+        when (model.states) {
+            1 -> {
                 item.findViewById<FloatingActionButton>(R.id.fab).backgroundTintList =
                     ColorStateList.valueOf(App.context.resources.getColor(R.color.green_2))
                 item.findViewById<LinearLayout>(R.id.linear)
@@ -49,7 +51,7 @@ class SectionAdapter( private var list: List<Level> , listener : Listener) :
                 item.findViewById<FloatingActionButton>(R.id.fab)
                     .setImageDrawable(App.context.resources.getDrawable(R.drawable.ic_check))
             }
-            2->{
+            2 -> {
                 item.findViewById<FloatingActionButton>(R.id.fab).backgroundTintList =
                     ColorStateList.valueOf(App.context.resources.getColor(R.color.orang))
                 item.findViewById<LinearLayout>(R.id.linear)
@@ -57,7 +59,7 @@ class SectionAdapter( private var list: List<Level> , listener : Listener) :
                 item.findViewById<FloatingActionButton>(R.id.fab)
                     .setImageDrawable(App.context.resources.getDrawable(R.drawable.ic_lock_on))
             }
-            else ->{
+            else -> {
                 item.findViewById<FloatingActionButton>(R.id.fab).backgroundTintList =
                     ColorStateList.valueOf(App.context.resources.getColor(R.color.gray))
                 item.findViewById<LinearLayout>(R.id.linear)
@@ -67,10 +69,9 @@ class SectionAdapter( private var list: List<Level> , listener : Listener) :
             }
         }
 
+        
+        item.setOnClickListener {
 
-
-                item.setOnClickListener {
-                listener.onClickListener(model)
         }
     }
 
@@ -79,14 +80,14 @@ class SectionAdapter( private var list: List<Level> , listener : Listener) :
     }
 
 
-
-    fun updateList(items: ArrayList<Level>){
+    fun updateList(items: ArrayList<Level>) {
         this.list = items
         notifyDataSetChanged()
     }
 
 
-    class MyViewHolder(private val itemBinding: ListItemSectionBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+    class MyViewHolder(itemBinding: ListItemSectionBinding) :
+        RecyclerView.ViewHolder(itemBinding.root) {
     }
 
 }
